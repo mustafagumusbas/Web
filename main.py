c61ed49fe587b4a8f2176cc9b4ea30a3d907a931
Mustafa Naim Gümüşbaş <mustafanaimgumusbas@gmail.com>
2024-11-07 01:01:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<a href="/şifre">Şifre Oluşturucuyu görüntüle</a><a href="/sayi">10 la 0 arası bir sayi tut </a> <h1>Ana Sayfa</h1>'
+    return '<a href="/şifre">Şifre Oluşturucuyu görüntüle</a>           <a href="/sayia">10 la 0 arası bir sayi tut </a> <h1>Ana Sayfa</h1>'
 
 @app.route("/şifre")
 def sifreweb():
     return f'<a href="/">Ana sayfayı görüntüle</a> <h1>Şifre Oluşturucu</h1><h3>{sifreyapici(8)}</h3>'
 
-@app.route("/sayi")
+@app.route("/sayia")
 def sayiweb():
-    return f'<a href="/">Ana sayfayı görüntüle</a> <h1>Çıkan Sayı : </h1> <h3>{sayi_tut}</h3>'
+    return f'<a href="/">Ana sayfayı görüntüle</a> <h1>Çıkan Sayı : {sayi_tut()}</h1>'
 
 
 app.run(debug=True)
+
 
